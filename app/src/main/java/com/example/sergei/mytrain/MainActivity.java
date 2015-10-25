0ac80b83dae32aad94b3af926b72dbef7a250da8
saegga <sergei.muzalev94@mail.ru>
2015-10-25 11:54:27 -1200

    Initial commit

---
+++ app/src/main/java/com/example/sergei/mytrain/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.sergei.mytrain;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by sergei on 25.10.2015.
+ */
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/example/sergei/mytrain/fragment/MainFragment.java
@@ -0,0 +1,10 @@
+package com.example.sergei.mytrain.fragment;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by sergei on 25.10.2015.
+ */
+public class MainFragment extends Fragment {
+
+}
